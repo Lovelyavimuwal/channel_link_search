1cc0320e6bd7d06689d3b07e78f834eb09bd0b7c
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-08-06 23:55:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 18779212
+API_HASH     = "1f6b28d46b9851b73c80553f0d2f5ea5"
 BOT_TOKEN    = ""
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://dazyky:N2wX9zV57ceeslrl@cluster0.vgt2teh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1001658743981
+ADMIN        = 6143551688
+CHANNEL      = "@snap_leaks_2024"
