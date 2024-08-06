5f4260f780761e496c14aa3966e0b214e68e9501
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-08-07 00:58:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,20 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = "mongodb+srv://dazyky:N2wX9zV57ceeslrl@cluster0.vgt2teh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1001658743981
-ADMIN        = 6143551688
-CHANNEL      = "@snap_leaks_2024"
+# API_ID       = 
+# API_HASH     = ""
+# BOT_TOKEN    = ""
+# SESSION      = ""
+# DATABASE_URI = "mongodb+srv://dazyky:N2wX9zV57ceeslrl@cluster0.vgt2teh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# LOG_CHANNEL  = -1001658743981
+# ADMIN        = 6143551688
+# CHANNEL      = "@snap_leaks_2024"
+
+import os
+from os import environ
+
+API_ID       = int(environ.get("API_ID", "18779212"))
+API_HASH     = environ.get("API_HASH", "1f6b28d46b9851b73c80553f0d2f5ea5")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+SESSION      = environ.get("SESSION", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dazyky:N2wX9zV57ceeslrl@cluster0.vgt2teh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1001658743981"))
+ADMIN        = int(environ.get("ADMIN", "6143551688"))
+CHANNEL      = environ.get("CHANNEL", "@YouTube_Sutra_Coolmitra_Abbyvira")
